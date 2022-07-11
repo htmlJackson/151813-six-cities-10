1237ced2750cd7794a79fabd54f5c949f0acda13
evgen <fiend.jackson@gmail.com>
2022-07-11 18:04:48 +0300

    выполнил задание по jsx

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+
+const cardsCount = 5;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsCount={cardsCount} />
   </React.StrictMode>,
 );
