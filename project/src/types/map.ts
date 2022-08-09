e4e6ffd968f454a262854585b30d5af08e8b1f3a
evgen <fiend.jackson@gmail.com>
2022-08-09 13:19:19 +0300

    leaflet

--- project/src/types/map.ts
+++ project/src/types/map.ts
@@ -1,14 +1,14 @@
 export type City = {
-    title: string;
-    lat: number;
-    lng: number;
-    zoom: number;
-  };
+  title: string;
+  lat: number;
+  lng: number;
+  zoom: number;
+};
   
   export type Point = {
-    title: string;
-    lat: number;
-    lng: number;
-  };
+  title: string;
+  lat: number;
+  lng: number;
+};
   
-  export type Points = Point[];
+export type Points = Point[];
