e537dddb3fdd779660995eee47479e5aae1a982c
evgen <fiend.jackson@gmail.com>
2022-08-21 20:26:13 +0300

    module7-task1

--- project/src/const.tsx
+++ project/src/const.tsx
@@ -3,13 +3,13 @@ export enum AppRoute {
   Favorites = '/favorites',
   Room = '/offer',
   Root = '/',
-};
+}
 
 export enum AuthorizationStatus {
   Auth = 'AUTH',
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
-};
+}
 
 export const URL_MARKER_DEFAULT =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
@@ -21,4 +21,4 @@ export const citiesList = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg
 
 export enum APIRoute {
   Offers = '/hotels',
-};
+}
